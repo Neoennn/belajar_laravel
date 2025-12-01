b1366a34c2d7aa0a288a125e2fb7df10ba08735c
Neoennn <149547396+Neoennn@users.noreply.github.com>
2025-12-01 16:48:23 +0700

    PKL
    Day 1 Latihan PKL

---
+++ belajar_laravel/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('Halo', function() {
+    return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+
+Route::get('dosen', [DosenController::class, 'index']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+Route::get('/pegawai', [PegawaiController::class, 'index']);
+
+
